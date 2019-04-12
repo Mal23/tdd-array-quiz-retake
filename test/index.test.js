ebcbe0afa00885f2f4c03938f508b38f5cca16a0
Mallarie <mallariemilano@gmail.com>
2019-04-12 15:43:09 -0700

    still working on function to pass test

--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,11 @@
+const numberMuncher = require('../lib/index');
+
 describe('numberMuncher function', () => {
   it('removes any numbers less than 5', () => {
     const input = [1, 3, 5, 7, 2];
     const expected = [1, 3, 2];
+    const result = numberMuncher(input);
 
-    expect(numberMuncher(input)).toEqual(expected);
+    expect(result).toEqual(expected);
   });
 });
